064d995bdf5de50d6aba46d4a65e625392a4b0ca
Julia Smith <42456804+pick7@users.noreply.github.com>
2025-10-15 22:54:07 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,17 @@
 // 存储当前的认证信息
 let authCredentials = null;
 
+// 启动时从storage恢复认证信息
+chrome.storage.sync.get(['proxyEnabled', 'authUsername', 'authPassword'], function(result) {
+  if (result.proxyEnabled && result.authUsername && result.authPassword) {
+    authCredentials = {
+      username: result.authUsername,
+      password: result.authPassword
+    };
+    console.log('代理认证信息已自动恢复');
+  }
+});
+
 // 监听来自popup的消息，获取认证信息
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'setAuth') {
@@ -8,8 +19,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       username: message.username,
       password: message.password
     };
+    // 同时保存到storage，以便浏览器重启后恢复
+    chrome.storage.sync.set({
+      authUsername: message.username,
+      authPassword: message.password
+    });
   } else if (message.type === 'clearAuth') {
     authCredentials = null;
+    // 清除storage中的认证信息
+    chrome.storage.sync.remove(['authUsername', 'authPassword']);
   }
 });
 
